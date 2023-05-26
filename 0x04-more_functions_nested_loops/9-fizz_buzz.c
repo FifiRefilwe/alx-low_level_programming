aec9c51c2e675c1019156bb18ccf5504343bd3df
FifiRefilwe <fifimia27@gmail.com>
2023-05-26 05:35:15 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,8 +2,8 @@
 
 /**
  * main - prints the numbers from 1 to 100, followed by a new line
- * but for multiples of three prints fizz instead of the number
- * and for the multiples of five prints buzz
+ * but for multiples of three prints Fizz instead of the number
+ * and for the multiples of five prints Buzz
  * Return: always 0 (success)
  */
 int main(void)
