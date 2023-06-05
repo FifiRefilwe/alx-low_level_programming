0d760695a66c21a77872878794d0e0f6c4ea19a3
FifiRefilwe <fifimia27@gmail.com>
2023-06-05 13:43:31 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,19 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-<<<<<<< HEAD
-int _putchar(char c);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
-=======
 #include <stdio.h>
 #include <unistd.h>
 
@@ -28,6 +15,5 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
->>>>>>> 2cd7e6b0bdd276cd938ba11344c82f451979512f
 
 #endif
