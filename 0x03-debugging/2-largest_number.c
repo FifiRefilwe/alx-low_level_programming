bb6dd7a0848d3daade5268a2d5f8a534191cf0c1
FifiRefilwe <fifimia27@gmail.com>
2023-06-05 10:54:03 -0700

    2-largest_number.c, main.h

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,7 +12,7 @@ int largest_number(int a, int b, int c)
 {
 int largest;
 
-if (a > b && b > c)
+if (a > b && a > c)
 {
 	largest = a;
 }
@@ -23,6 +23,10 @@ else if (b > c && b > a)
 else if (c > b)
 {
 	largest = c;
+}
+else
+{
+	largest = b;
 }
 
 return (largest);
