39df81d2b44d73948e03a0f09a08c506d7964469
FifiRefilwe <fifimia27@gmail.com>
2023-06-06 09:42:39 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
+ * main - 4-puts.c
+ *description - write a c program that prints exactly
+ * 
  * Return: Always 0 (success)
  */
 int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
+
 	return (0);
 }
