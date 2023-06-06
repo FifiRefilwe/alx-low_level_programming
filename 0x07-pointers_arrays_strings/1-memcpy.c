07fbf6b5b324d7cda141610c3068d6d5c84f6c3d
FifiRefilwe <fifimia27@gmail.com>
2023-06-06 03:30:06 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memset - Entry point
+ * @s: pointed destination
+ * @b: constant byte
+ * @n: bytes
+ * Return: Always 0 (success)
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	return (dest);
+}
+
