50cd3bd41a0b4a649e7025ac8b16c5577de39c95
FifiRefilwe <fifimia27@gmail.com>
2023-08-30 11:25:57 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,32 +1,22 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * main - check the code
+ * listint_len - the number of elements in a linked listint_t list.
  *
- * Return: Always 0.
+ * @h: head pointer
+ *
+ * Return: the number of elements
+ *
  */
-int main(void)
-{
-    listint_t *head;
-    listint_t *new;
-    listint_t hello = {8, NULL};
-    size_t n;
 
-    head = &hello;
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-    {
-        printf("Error\n");
-        return (1);
-    }
-    new->n = 9;
-new->next = head;
-head = new;
-n = listint_len(head);
-printf("-> %lu elements\n", n);
-free(new);
-return (0);
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+	h = h->next;
+		count++;
+	}
+	return (count);
 }
